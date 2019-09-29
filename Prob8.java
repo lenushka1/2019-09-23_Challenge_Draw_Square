37d5d19a850b32bc304ebf4b4d4bfd383db26c30
lenushka1 <55368081+lenushka1@users.noreply.github.com>
2019-09-28 20:22:21 -0400

    Create Prob8.java

---
+++ Prob8.java
@@ -0,0 +1,7 @@
+public class Prob8 {
+    public static void main (String [] args) {
+        int radius = 17;
+        double pi = 3.14;
+        System.out.println (pi * radius * radius);
+    }
+}
